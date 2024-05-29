d2772617d928227f8d6f79c15b29899cb74ad317
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-05-29 06:01:48 +0100

    first commit

---
+++ src/test/java/com/zee/springmodulithcourse/SpringModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.springmodulithcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
