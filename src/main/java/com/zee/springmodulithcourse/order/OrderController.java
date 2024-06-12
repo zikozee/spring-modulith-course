e7f0c60caa7ed6c831cc97071be67a3224b4713e
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-06-12 06:52:33 +0100

    commit Order Controller closes #18

---
+++ src/main/java/com/zee/springmodulithcourse/order/OrderController.java
@@ -0,0 +1,36 @@
+package com.zee.springmodulithcourse.order;
+
+import com.zee.springmodulithcourse.order.dto.CompleteOrder;
+import com.zee.springmodulithcourse.order.dto.CompleteOrderResponse;
+import com.zee.springmodulithcourse.order.dto.OrderDto;
+import com.zee.springmodulithcourse.order.dto.OrderResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Ezekiel Eromosei
+ * @code @created : 30 May, 2024
+ */
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "order")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@RequestBody @Valid OrderDto orderDto) {
+        return new ResponseEntity<>(orderService.createOrder(orderDto), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "complete")
+    public ResponseEntity<CompleteOrderResponse> completeOrder(@RequestBody @Valid CompleteOrder completeOrder) {
+        return new ResponseEntity<>(orderService.completePayment(completeOrder), HttpStatus.OK);
+    }
+}
